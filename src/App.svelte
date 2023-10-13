2fe08868abfc97398179f051df83f537ca600303
Logan Woolf <loganwoolf@gmail.com>
2023-10-13 06:32:18 +0000

    WIP Add app content

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,40 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  let term: string = ''
+
+  $: definition = getDefinition(term)
+
+
+  async function getDefinition (term) {
+    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${term}`)
+    const text = await response.text()
+    console.log({text})
+  }
+
+  function clearTerm() {
+    term = ''
+  }
 </script>
 
+<header>
+  <h1>Lightspeed Dictionary</h1>
+  <p>Redefining fast on every keypress</p>
+</header>
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <form class="form">
+    <input bind:value={term} type="text" autocomplete="off">
+    <button on:click={clearTerm} type="button">Clear</button>
+  </form>
+  <div class="result">
+    {#await definition}
+
+    {:then }
+
+    {/await}
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
+<footer>
+  <p>Made in 🍁 by Logan Woolf</p>
+</footer>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
